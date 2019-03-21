7ff0f5c435d810b706b6466603ec77dce69fa367
Unknown <loison.dorian@gmail.com>
2019-03-21 15:18:09 +0100

    [model] ajout organisation

---
+++ app/models/Organisation.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import play.db.jpa.GenericModel;
+
+@Entity
+public class Organisation extends GenericModel {
+
+	@Column(length=100,nullable=false)
+	public String nom;
+
+	@Column(columnDefinition="text",nullable=false)
+	public String adresse;
+	
+	@Column(columnDefinition="text")
+	public String commentaire;	
+
+	@Column(columnDefinition="datetime",nullable=false)
+	public Date dateAjout;
+	
+	@Column(columnDefinition="datetime")
+	public Date dateModification;
+	
+	@ManyToOne
+	public Pays pays;
+	
+	@ManyToOne
+	public Civil dirigents;
+	
+
+	@ManyToMany
+	public List<Civil> membres;
+}
