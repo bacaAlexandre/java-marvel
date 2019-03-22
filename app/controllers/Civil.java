a9d3d432ab681f69e46a226bdd2653df6ca3fb25
Unknown <loison.dorian@gmail.com>
2019-03-22 09:33:02 +0100

    [controller] ajout controller civil

---
+++ app/controllers/Civil.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Civil extends Controller {
+
+	public static void index() {
+	    render();
+	}
+	
+	public static void addCivil() {
+        render();
+    }
+	
+}
