1633f8f72585f5d25032d023bb1920a0c62370e6
Moi <jeremy-emo@hotmail.fr>
2019-04-17 10:03:02 +0200

    feat : index incidents

--- app/controllers/IncidentController.java
+++ app/controllers/IncidentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lib.Genform;
 import models.Civil;
 import models.Incident;
+import models.Organisation;
 import models.TypeDelit;
 import models.Utilisateur;
 import play.data.validation.Valid;
@@ -12,26 +13,36 @@ import play.mvc.Controller;
 
 public class IncidentController extends Controller {
 
+	public static void index() {
+		Utilisateur utilisateur = AuthController.connected();
+		if (utilisateur.can("IncidentController", "read")) {
+			List<Incident> incidents = Incident.find("byMissionIsNull").fetch();
+			render("IncidentController/index.html", incidents);
+		}
+		redirect("/");
+	}
+
 	public static void declaration() {
 		Utilisateur utilisateur = AuthController.connected();
 		if (utilisateur.can("IncidentController", "create")) {
 			List<TypeDelit> delits = TypeDelit.findAll();
-			String form = new Genform(new Incident(), "/incident/declaration", "crudform").generate(validation.errorsMap(), flash);
-		    render("IncidentController/declaration.html", delits, form);
+			String form = new Genform(new Incident(), "/incident/declaration", "crudform")
+					.generate(validation.errorsMap(), flash);
+			render("IncidentController/declaration.html", delits, form);
 		}
 		redirect("/");
 	}
-	
+
 	public static void declarer(@Valid Incident incident, long typeDelit) {
 		Utilisateur utilisateur = AuthController.connected();
 		if (utilisateur.can("IncidentController", "create")) {
 			incident.typeDelit = TypeDelit.findById(typeDelit);
 			incident.civil = utilisateur.civil;
-			if(validation.hasErrors()) {
-	            params.flash();
-	            validation.keep();
-	            declaration();
-	        }
+			if (validation.hasErrors()) {
+				params.flash();
+				validation.keep();
+				declaration();
+			}
 			incident.save();
 		}
 		declaration();
